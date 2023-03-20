6fa6afd661fcf6c2a47946c49ddc3d05cbe3938c
Boqun Feng <boqun.feng@gmail.com>
2023-03-20 11:02:33 -0700

    hyperv: mshv_vtl: Add tracepoints for VTL0 enter and exit
    Signed-off-by: Boqun Feng <boqun.feng@gmail.com>

--- arch/x86/include/asm/trace/hyperv.h
+++ arch/x86/include/asm/trace/hyperv.h
@@ -86,6 +86,33 @@ TRACE_EVENT(hyperv_send_ipi_one,
 		      __entry->cpu, __entry->vector)
 	);
 
+#if IS_ENABLED(CONFIG_MSHV_VTL)
+TRACE_EVENT(mshv_vtl_exit_vtl0,
+	    TP_PROTO(u32 vtl_entry_reason, void *ctx),
+	    TP_ARGS(vtl_entry_reason, ctx),
+	    TP_STRUCT__entry(
+		    __field(u32, vtl_entry_reason)
+		    __field(void*, ctx)
+		    ),
+	    TP_fast_assign(__entry->vtl_entry_reason = vtl_entry_reason;
+			   __entry->ctx = ctx;
+		    ),
+	    TP_printk("vtl2 entry reason %u, cpu_ctx=%p",
+		      __entry->vtl_entry_reason, __entry->ctx)
+	   );
+
+TRACE_EVENT(mshv_vtl_enter_vtl0,
+	    TP_PROTO(void *ctx),
+	    TP_ARGS(ctx),
+	    TP_STRUCT__entry(
+		    __field(void*, ctx)
+		    ),
+	    TP_fast_assign(__entry->ctx = ctx;
+		    ),
+	    TP_printk("cpu_ctx=%p", __entry->ctx)
+	   );
+#endif /* CONFIG_MSHV_VTL */
+
 #endif /* CONFIG_HYPERV */
 
 #undef TRACE_INCLUDE_PATH
